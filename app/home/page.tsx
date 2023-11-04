d8eb024f41c2a9dcc19d5a42b1023a54f656907a
Gaurav Chaughule <gaurav.chaughule@ampcustech.com>
2023-11-04 10:30:59 +0530

    Chat screen made responsive

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,7 +3,7 @@ import { ChatBotComponent } from "@/components/ChatBotComponent";
 export default function Home() {
   return (
     <div className='w-full h-full flex justify-center items-center'>
-        <div className="w-[50%] h-[96%]">
+        <div className="lg:w-[50%] md:w-[50%] xs:w-full h-[96%]">
           <ChatBotComponent/>
         </div>
     </div>
