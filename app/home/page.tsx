31e85e26957a102c950ff82377be09007c6d110a
loknat prajapati <loknath.prajapati@ampcustech.com>
2023-11-06 19:55:08 +0530

    Conflicts resolved

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,10 +4,10 @@ import VoiceAssistant from "@/components/VoiceAssistant";
 
 export default function Home() {
   return (
-    <div className='w-full h-full flex justify-center items-center'>
-        <div className="lg:w-[50%] md:w-[50%] xs:w-full h-[96%]">
-          <ChatBotComponent/>
-        </div>
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="lg:w-[50%] md:w-[50%] xs:w-full h-[96%]">
+        <ChatBotComponent />
+      </div>
     </div>
   );
 }
